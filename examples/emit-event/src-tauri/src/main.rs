aa56cad6505a6fdf6ab426022ee204d82da4cbdf
wkl007 <499657357@qq.com>
2022-09-20 10:44:52 +0800

    chore: upgrade tauri 1.1.1

--- examples/emit-event/src-tauri/src/main.rs
+++ examples/emit-event/src-tauri/src/main.rs
@@ -3,12 +3,22 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{Manager};
+use tauri::{Manager, SystemTray, SystemTrayMenu, CustomMenuItem};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
   args: Vec<String>,
   cwd: String,
+}
+
+fn system_tray() -> SystemTray {
+  let open = CustomMenuItem::new("open".to_string(), "open");
+  let quit = CustomMenuItem::new("quit".to_string(), "quit");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(open)
+    .add_item(quit);
+  let tray = SystemTray::new().with_menu(tray_menu);
+  return tray;
 }
 
 fn main() {
@@ -17,6 +27,7 @@ fn main() {
       println!("{}, {argv:?}, {cwd}", app.package_info().name);
       app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
     }))
+    .system_tray(system_tray())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
